11eee43b3ffc23326925f666a320a76600cfebed
caijinxu <enzof2004@163.com>
2017-03-30 01:36:18 +0800

    updata

---
+++ S12/day1/file_open.py
@@ -0,0 +1,28 @@
+
+'''
+f = open("test.log","w")
+
+f.write("第一行\n")
+f.write("第2行\n")
+f.write("第3行\n")
+f.write("最后一行\n")
+
+f.close()
+
+'''
+
+'''
+f = open("test.log","r")
+for line in f:
+    if "3" in line:
+        print ("this is the 3 line")
+    else:
+        print (line),
+f.close()
+'''
+'''
+f = open("test.log","w")
+f.write("8")
+f.write("9")
+f.close()
+'''
